2f58dfe83ee62200c556926917b6ebf7d7e6f363
yassine <yassinebichiou2@gmail.com>
2026-04-28 19:59:55 +0100

    feat(theme): add info color to AppTheme palette

--- lib/utils/app_theme.dart
+++ lib/utils/app_theme.dart
@@ -8,6 +8,7 @@ class AppTheme {
   static const Color success = Color(0xFF10B981);
   static const Color warning = Color(0xFFF59E0B);
   static const Color error = Color(0xFFEF4444);
+  static const Color info = Color(0xFF06B6D4);
 
   // Light surfaces
   static const Color bgLight = Color(0xFFF1F5F9);
